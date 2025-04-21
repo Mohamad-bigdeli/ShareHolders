fb02ab8556194682b10a167cea6f365505278372
Mohamad-bigdeli <mohamadbigdeli24@gmail.com>
2025-04-22 00:43:30 +0330

    add jalali date in serializer

--- core/shareholder/api/v1/serializers.py
+++ core/shareholder/api/v1/serializers.py
@@ -14,6 +14,7 @@ class ShareholdersListSerializer(serializers.ModelSerializer):
         fields = [
             "id", 
             "date",
+            "jalali_date",
             "shareholder_id",
             "shareholder_name",
             "shareholder_percentage",
